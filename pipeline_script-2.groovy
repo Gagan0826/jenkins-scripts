a62f618c67549b364cf6d9721e63ee446dc1b136
Gagan0826 <63014073+Gagan0826@users.noreply.github.com>
2023-07-21 22:57:22 +0530

    Create pipeline_script-2.groovy

---
+++ pipeline_script-2.groovy
@@ -0,0 +1,33 @@
+pipeline {
+    agent any
+    stages {
+        stage("Install Apache2") {
+            steps {
+                sh 'sudo apt update' // Run apt update before installing Apache2
+                sh 'sudo apt install apache2 -y'
+            }
+        }
+        stage("Print Working Directory") {
+            steps {
+                sh 'pwd'
+            }
+        }
+        stage("Git Clone") {
+            steps {
+                sh 'rm -rf jenkins-test-repo-1'
+                sh 'git clone https://Gagan_br_ITT@bitbucket.org/dvoptrain/jenkins-test-repo-1.git'
+                sh 'cd jenkins-test-repo-1'
+                sh 'ls'
+            }
+        }
+        stage("Copy and Host to Apache2") {
+            steps {
+                
+                sh 'cd jenkins-test-repo-1/HTML-personal-site-master/'
+                sh 'ls'
+                sh 'sudo cp -r /var/lib/jenkins/workspace/pipeline-2/jenkins-test-repo-1/HTML-personal-site-master/* /var/www/html'
+                // sh 'sudo chown -R www-data:www-data /var/www/html/jenkins-test-repo-1'
+            }
+        }
+    }
+}
